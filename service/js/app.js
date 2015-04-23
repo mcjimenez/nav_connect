9b113d6f7972ef4edfc2db2dca5ed8a572ace1c0
Carmen Jimenez Cabezas <cjc@tid.es>
2015-04-23 16:40:29 +0200

    added service web app

---
+++ service/js/app.js
@@ -0,0 +1,85 @@
+(function() {
+  'use strict';
+
+  function debug(str) {
+    console.log("CJC -*-:" + str);
+  }
+
+  // This is a very basic sample app that uses a SW and acts as a server for
+  // navigator.connect. I'm going to mark with a comment where the app MUST
+  // add some extra code to use the navigator.connect SHIM
+  // So if you just want to know that, search for:
+  // ADDED FOR SHIM
+
+  var register = function(evt) {
+    debug('APP executing register...');
+    navigator.serviceWorker.register('/nav_connect/service/sw.js', {scope: './'}
+    ).then(function(reg) {
+      debug('APP Registration succeeded. Scope: ' + reg.scope);
+      if (reg.installing) {
+        debug('APP registration --> installing');
+      } else if (reg.waiting) {
+        debug('APP registration --> waiting');
+      } else if (reg.active) {
+        debug('APP registration --> active');
+      }
+    }).catch(function(error) {
+      debug('APP Registration failed with ' + error);
+    });
+  };
+
+  var unregister = function(evt) {
+    debug('APP Unregister...');
+    navigator.serviceWorker.getRegistrations().then(regs => {
+      regs.forEach(reg => {
+        reg.unregister();
+        debug('APP Unregister done');
+      });
+    });
+  };
+
+  var processSWRequest = function(channel, evt) {
+    // Your code here
+    // from this point on, you would write your handler as if the shim wasn't
+    // present.
+    var sett = evt.data.setting;
+    if (!sett) {
+      // Return no setting msg
+      return;
+    }
+
+    var _settings = navigator.mozSettings;
+    _settings.createLock().get(sett).then(result => {
+      debug('APP value: ' + result[sett] + ' send to sw');
+      channel.postMessage({'setting': sett,
+                           'value': result[sett]});
+    });
+  };
+
+  if ('serviceWorker' in navigator) {
+    debug('APP serviceWorker in navigator');
+    register();
+    navigator.serviceWorker.ready.then(sw => {
+      // Let's pass the SW some way to talk to us...
+      var mc = new MessageChannel();
+      mc.port1.onmessage = processSWRequest.bind(this, mc.port1);
+      sw.active && sw.active.postMessage({}, [mc.port2]);
+    });
+  } else {
+    debug('APP navigator does not have ServiceWorker');
+    return;
+  }
+
+  // Testing purpose only!!!!
+  window.addEventListener('load', function () {
+    var regBto = document.querySelector('#regBto');
+    var unRegBto = document.querySelector('#unregBto');
+    var sendMessageBto = document.querySelector('#sendMsgBto');
+    var connBto = document.querySelector('#connMsgMto');
+    regBto.addEventListener('click', register);
+    unRegBto.addEventListener('click', unregister);
+    sendMessageBto.addEventListener('click',
+      { appicon : 'app://sms.gaiamobile.org/style/icons/sms_126.png'});
+  });
+
+})(self);
---
+++ service/sw.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// This is a very basic sample Service Worker (SW) that  acts as a server for
+// navigator.connect. I'm going to mark with a comment where the app MUST
+// add some extra code to use the navigator.connect POLYFILL
+// So if you just want to know that, search for:
+// ADDED FOR POLYFILL
+
+function debug(str) {
+  console.log('CJC -*- -->' + str);
+}
+
+// ADDED FOR POLYFILL: Import the polyfill script
+this.importScripts('/swpolyfill/polyfill/navigator_connect_sw.js');
+// END ADDED FOR POLYFILL
+
+this.addEventListener('install', function(evt) {
+  debug('SW Install event');
+});
+
+this.addEventListener('activate', function(evt) {
+  debug('SW activate event');
+});
+
+this.addEventListener('fetch', function(evt) {
+  debug('SW fetch event');
+});
+
+this.channelToMT = new Promise((resolve, reject) => {
+  this.resolveChannel = resolve;
+  this.rejectChannel = reject;
+});
+
+this.onconnect = function(msg) {
+  debug('SW onconnect: We should have a port here on msg.source. ' +
+        (msg.source.postMessage ? 'yes!' : 'no :('));
+  // msg.source should have the endpoint to send and receive messages,
+  // so we can do:
+  msg.acceptConnection(true);
+  msg.source.onmessage = aMsg => {
+    debug('SW SETTING msg received:' + JSON.stringify(aMsg.data));
+    var setting = aMsg.data.setting;
+    if (setting) {
+      debug('SW SETTING requested setting:' + setting);
+      // In sw APIS do not work!!!! We need to request it to the main thread
+
+      // Since this doesn't work the first time, and we don't want to have to
+      // do a reload, we'll work around this by making the main thread pass
+      // us a MessageChannel to talk to it
+      this.channelToMT.then(channel => {
+        // This has a problem... we can't queue messages
+        channel.onmessage = evt => {
+          msg.source.postMessage(evt.data);
+        };
+        channel.postMessage({ 'setting': setting });
+      });
+
+    } else {
+      debug('SW Got a message from one of the accepted connections: ' +
+            JSON.stringify(aMsg.data));
+      msg.source.postMessage('Hello, client! I got your request: ' +
+                             JSON.stringify(aMsg.data));
+    }
+  };
+};
+
+this.messageListener = evt => {
+  // This is a hack caused by the lack of dedicated MessageChannels... sorry!
+  debug('SW onmessage ---> '+ JSON.stringify(evt.data));
+  // ADDED FOR POLYFILL
+  // Since we're using the same channel to process messages comming from the
+  // main thread of the app to the SW, and messages coming from the
+  // navigator.connect polyfill, we have to distinguish them here. Sadly we
+  // can't remove this even if we have MessageChannels because we have to pass
+  // the MessageChannels down (connection messages) somehow.
+  if (this.NCPolyfill.isInternalMessage(evt)) {
+    debug('SW msg is internal, do not process');
+    return;
+  }
+  // END ADDED FOR POLYFILL
+
+  // Your code here
+  // The only message we should get here is a MessageChannel to talk back to
+  // the main thread... so...
+  if (evt.ports && evt.ports[0]) {
+    debug('Got a channel from the parent');
+    this.resolveChannel(evt.ports[0]);
+  } else {
+    debug('Did not got a channel!');
+    this.rejectChannel('I did not got a channel');
+  }
+  // And I can remove the listener, I don't need this anymore
+  this.removeEventListener('message', this.messageListener);
+
+};
+
+this.addEventListener('message', this.messageListener);
+
