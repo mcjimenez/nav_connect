8a2d15a17926f106fe3dd06a77bdcb0910f3cd26
Carmen Jimenez Cabezas <cjc@tid.es>
2015-05-05 19:13:42 +0200

    testing noAllowed

--- client/js/clientConnect.js
+++ client/js/clientConnect.js
@@ -57,7 +57,7 @@
     }).catch(
       error => {
         debug('CLIENT connection refused:' + JSON.stringify(error));
-        _addTxt('navigator.connect failed. ' + error);
+        _addTxt('navigator.connect failed. ' + JSON.strigify(error), whatEntry);
       }
     );
 
