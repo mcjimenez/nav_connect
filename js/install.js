082ba1e395322e3b6ba0097ad76286f16072a304
Carmen Jimenez Cabezas <cjc@tid.es>
2015-07-06 14:52:09 +0200

    Added installer

---
+++ js/install.js
@@ -0,0 +1,26 @@
+(function(window) {
+  'use strict';
+
+  var origin = document.location.origin;
+  var _btoInstallClt = document.getElementById('installClt');
+  var _btoInstallSvr1 = document.getElementById('installSvr1');
+  var _btoInstallSvr2 = document.getElementById('installSvr2');
+  var I = 'nav_connect';
+
+  function install(what) {
+    var origin = document.location.origin;
+    // Could get this from the href but not really worth the hassle
+    var realPath = origin + '/' + I + '/' + what + '/manifest.webapp';
+adb.logcat('CJC:' + realPath);
+    navigator.mozApps.install(realPath);
+  }
+
+  window.addEventListener('load', function() {
+    _btoInstallClt.addEventListener('click', install.bind(null, 'client'));
+    _btoInstallSvr1.addEventListener('click',
+                                     install.bind(null, 'service'));
+    _btoInstallSvr2.addEventListener('click',
+                                     install.bind(null, 'service2'));
+  });
+
+})(window);
